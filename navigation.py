0c8906656876b60302e9d49b7f1f3a1372d38558
Bchri022 <Bchri022@fiu.edu>
2022-11-07 19:07:00 -0500

    Navigation class completed

---
+++ navigation.py
@@ -0,0 +1,21 @@
+""
+#This is the navigation class 
+""
+class Navigation:
+
+    def __init__(self, bluetooth, location, guidance) -> None:
+        self._bluetooth = bluetooth #self. makes it an attribute
+        self._location = location # _location says location should be private
+        self._guidance = guidance
+
+        def  stop(self):
+             print("Make the robot Stop")
+        ""
+        #Method to make the robot stop/go when robots around are operating
+        def go(self):
+            print("Make the robot Go")
+    
+    
+
+
+        
